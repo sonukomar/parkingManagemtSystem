a4327ecd92dcc71c5d4271936d6ab9fe8f601038
sonukomar <enggsonukomar@gmail.com>
2017-09-07 01:31:27 +0530

    admin.py modified

--- app/admin.py
+++ app/admin.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from models import *
+from app.models import *
 # Register your models here.
 
 admin.site.register(vehicle)
